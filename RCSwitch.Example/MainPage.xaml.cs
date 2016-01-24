d7dd7a901b5dbe7c050c029ffb4c411ece236108
Michael Osthege <Windows Live ID\zufallsprinzip@hotmail.de>
2016-01-24 17:42:19 +0100

    core, test, example and nuget

---
+++ RCSwitch.Example/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+namespace RCSwitch.Example
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RCSwitchIO rcSwitch = new RCSwitchIO(6, 5); // connect sender to GPIO6 and receiver to GPIO5
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            StartReceiving();
+        }
+
+        private void On_Click(object sender, RoutedEventArgs e)
+        {
+            rcSwitch.Switch(codeGroupTB.Text, codeDeviceTB.Text, true);
+        }
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            rcSwitch.Switch(codeGroupTB.Text, codeDeviceTB.Text, true);
+        }
+
+        private async void StartReceiving()
+        {
+            await System.Threading.Tasks.Task.Delay(20);
+            while (true)
+            {
+                if (rcSwitch.available())
+                {
+                    System.Diagnostics.Debug.WriteLine($"received: {rcSwitch.getReceivedCode()}");
+                    rcSwitch.resetAvailable();
+                }
+                await System.Threading.Tasks.Task.Delay(10);
+            }
+        }
+    }
+}
