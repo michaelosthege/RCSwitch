b60e0d499d6bb8ed33f4df8d019182dd8da42e26
Michael Osthege <Windows Live ID\zufallsprinzip@hotmail.de>
2016-02-08 14:54:43 +0100

    updated example

--- RCSwitch.Example/MainPage.xaml.cs
+++ RCSwitch.Example/MainPage.xaml.cs
@@ -13,13 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
-
 namespace RCSwitch.Example
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         RCSwitchIO rcSwitch = new RCSwitchIO(6, 5); // connect sender to GPIO6 and receiver to GPIO5
@@ -27,7 +22,12 @@ namespace RCSwitch.Example
         public MainPage()
         {
             this.InitializeComponent();
-            StartReceiving();
+            rcSwitch.OnSignalReceived += RcSwitch_OnSignalReceived;//attach the event handler for receiving signals
+        }
+
+        private void RcSwitch_OnSignalReceived(object sender, Signal signal)
+        {
+            System.Diagnostics.Debug.WriteLine($"received: {signal.Code} via protocol {signal.Protocol} with bitlength {signal.BitLength}");
         }
 
         private void On_Click(object sender, RoutedEventArgs e)
@@ -37,20 +37,6 @@ namespace RCSwitch.Example
         private void Off_Click(object sender, RoutedEventArgs e)
         {
             rcSwitch.Switch(codeGroupTB.Text, codeDeviceTB.Text, true);
-        }
-
-        private async void StartReceiving()
-        {
-            await System.Threading.Tasks.Task.Delay(20);
-            while (true)
-            {
-                if (rcSwitch.available())
-                {
-                    System.Diagnostics.Debug.WriteLine($"received: {rcSwitch.getReceivedCode()}");
-                    rcSwitch.resetAvailable();
-                }
-                await System.Threading.Tasks.Task.Delay(10);
-            }
         }
     }
 }
